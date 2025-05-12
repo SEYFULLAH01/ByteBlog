7226f48f9241c762e796b918f015cc6740f533ba
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-05-12 11:33:28 +0300

    İlişkisel tablolarda listeleme işlemleri ve auto mapper ile dto ilişkilerini kurma işlemlerini tamamladık

--- ByteBlog.Entity/DTOs/Articles/ArticleDto.cs
+++ ByteBlog.Entity/DTOs/Articles/ArticleDto.cs
@@ -1,12 +1,19 @@
-﻿namespace ByteBlog.Entity.DTOs.Articles
+﻿using ByteBlog.Entity.DTOs.Categories;
+using ByteBlog.Entity.Entities;
+
+namespace ByteBlog.Entity.DTOs.Articles
 {
     public class ArticleDto
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+        public CategoryDto Category { get; set; }
         public DateTime CreatedDate { get; set; }
+        public Image Image { get; set; }
+        public AppUser User { get; set; }
         public string CreatedBy { get; set; }
+        public bool IsDeleted { get; set; }
         public int ViewCount { get; set; }
     }
 }
---
+++ ByteBlog.Service/AutoMapper/Categories/CategoryProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ByteBlog.Entity.DTOs.Categories;
+using ByteBlog.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBlog.Service.AutoMapper.Categories
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryDto, Category>().ReverseMap();
+        }
+    }
+}
